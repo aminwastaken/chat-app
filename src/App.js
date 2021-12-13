47783477b2318526b698032412f5c2c925568e2e
amin <aminwastaken@gmail.com>
2021-12-13 18:25:41 +0100

    Implement router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import { getMessages, createMessage } from "./utils/firebase";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
 
 function App() {
   createMessage({ toto: "another toto" });
@@ -9,7 +11,15 @@ function App() {
     const messages = await getMessages();
     console.log(messages);
   })();
-  return <div>chat app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat />}>
+          <Route path="chat" element={<Chat />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
